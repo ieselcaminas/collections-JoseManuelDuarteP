c30dbb886a96b85d64373e4f68c8b83901461dfa
josduaper <josduaper@alu.edu.gva.es>
2024-11-29 10:04:21 +0100

    Telefonos

--- src/Telefonos2.java
+++ src/Telefonos2.java
@@ -5,30 +5,33 @@ import java.util.Scanner;
 
 public class Telefonos2 {
     public static void main(String[] args) {
-        String contactoUsu;
-        Scanner sc = new Scanner(System.in);
-        Map<String, ArrayList<String>> contactos = new HashMap<>();
-        ArrayList<String> telefonos = new ArrayList<>();
-        telefonos.add("11111");
-        telefonos.add("22222");
-        contactos.put("Jose", telefonos);
-        telefonos = new ArrayList<>();
-        telefonos.add("33333");
-        telefonos.add("44444");
-        contactos.put("Michael", telefonos);
-        telefonos = new ArrayList<>();
-        telefonos.add("55555");
-        telefonos.add("66666");
-        contactos.put("Bob", telefonos);
+        do {
+            String contactoUsu;
+            Scanner sc = new Scanner(System.in);
+            Map<String, ArrayList<String>> contactos = new HashMap<>();
+            ArrayList<String> telefonos = new ArrayList<>();
+            telefonos.add("11111");
+            telefonos.add("22222");
+            contactos.put("Jose", telefonos);
+            telefonos = new ArrayList<>();
+            telefonos.add("33333");
+            telefonos.add("44444");
+            contactos.put("Michael", telefonos);
+            telefonos = new ArrayList<>();
+            telefonos.add("55555");
+            telefonos.add("66666");
+            contactos.put("Bob", telefonos);
 
-        System.out.println("Introduzca un contacto" + "\n" + contactos.keySet());
-        contactoUsu = sc.nextLine();
+            System.out.println("Introduzca un contacto" + "\n" + contactos.keySet());
+            contactoUsu = sc.nextLine();
 
-        if (telefono(contactos, contactoUsu) == null) {
-            System.out.println("Contacto " + contactoUsu + " no exite");
-        } else {
-            System.out.println(telefono(contactos, contactoUsu));
-        }
+            if (telefono(contactos, contactoUsu) == null) {
+                System.out.println("Contacto " + contactoUsu + " no exite");
+            } else {
+                System.out.println(telefono(contactos, contactoUsu));
+                break;
+            }
+        } while (true);
     }
 
     public static ArrayList<String> telefono
