46b8ee0becf36bf364ee99905974263b2347a80c
josduaper <josduaper@alu.edu.gva.es>
2024-12-05 11:37:52 +0100

    Anagrams

---
+++ src/CountCountries.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class CountCountries {
+    public static void main(String[] args) throws IOException {
+        System.out.println(countCountries());
+    }
+
+    public static Map<String,Integer> countCountries () throws IOException {
+        BufferedReader lector = new BufferedReader(new FileReader
+                ("files/Colfuturo-Seleccionados.csv"));
+        String linea;
+        Map<String,Integer> mapa = new HashMap<>();
+
+        while ((linea = lector.readLine()) != null) {
+            String[] datos = linea.split(",");
+
+            if (mapa.containsKey(datos[6])) {
+                mapa.put(datos[6], mapa.get(datos[6]) + 1);
+            } else {
+                mapa.put(datos[6], 1);
+            }
+
+        }
+        lector.close();
+        return mapa;
+    }
+
+}
--- test/CountCountriesTest.java
+++ test/CountCountriesTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountCountriesTest {
